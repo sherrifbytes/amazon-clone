17484c1b0dac0e765f6e116483e407a9e7f34d0e
sherrifbytes <chidyess@gmail.com>
2020-10-20 21:29:08 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3OuEnAemESH7OlZCYbM6rETWnLUVZ58A",
+    authDomain: "challenge-36b6b.firebaseapp.com",
+    databaseURL: "https://challenge-36b6b.firebaseio.com",
+    projectId: "challenge-36b6b",
+    storageBucket: "challenge-36b6b.appspot.com",
+    messagingSenderId: "549252525086",
+    appId: "1:549252525086:web:b201c79a91b45c1dfe520f"
+};
