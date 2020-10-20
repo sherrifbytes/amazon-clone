a977953bdeb06a52358748606835b6ee8dc13690
sherrifbytes <chidyess@gmail.com>
2020-10-20 22:57:56 +0200

    structured header.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
     return ( <
         div className = "app" >
-            <h1>Hello coders, lets clone amazon....!!!</h1>  
+            <h1>Hello coders, lets clone amazon....!!!</h1>
+            {/* Header */}
+            {/* Home */}
         </div>
     );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import '.styles/Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            {/* amazon logo section */}
+            <img className="header__logo"
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+        
+            {/* searchbar on top */}
+            <div className="header__search">
+                <input className="header__searchInput" type="text"/>
+            </div>
+
+            {/* header nav */}
+            <div className="header__nav">
+                
+                {/* each nav item below */}
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>
+                        Hello Guest
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        Sign In
+                    </span>
+                </div>
+
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>
+                        Returns
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        & Orders
+                    </span>
+                </div>
+
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>
+                        Your
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        Prime
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
