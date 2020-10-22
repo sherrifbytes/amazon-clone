74825c2550bdade85d05b838335eabdcba73f4e9
sherrifbytes <chidyess@gmail.com>
2020-10-22 12:43:25 +0200

    home page components created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
     return ( <div className = "app">
 
-            {/* Header */}
+            /* Header */
             <Header />
-            {/* Home */}
+            /* Home */
+            <Home />
         </div>
     );
 }
